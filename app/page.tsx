087d347c58c16c0f1b9fd26b729ba800783fba1c
Muhammad Abdullah <muhammadabdullahfscem@gmail.com>
2024-08-24 20:01:23 +0500

    Update Navbar component with navigation links

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-import Navbar from '../components/Navbar'
-import AboutUs from '../components/AboutUs';
-import Hero from '../components/Hero';
-import OurServices from '../components/OurServices';
-import MyProjects from '../components/MyProjects'; // Corrected import statement
-import ContactMe from '../components/ContactMe';
+import Navbar from '../components/PortfolioTemplate-5/Navbar';
+import AboutUs from '../components/PortfolioTemplate-5/AboutUs';
+import Hero from '../components/PortfolioTemplate-5/Hero';
+import OurServices from '../components/PortfolioTemplate-5/OurServices'; // Ensure this path is correct
+import MyProjects from '../components/PortfolioTemplate-5/MyProjects'; 
+import ContactMe from '../components/PortfolioTemplate-5/ContactMe';
+
 export default function Home() {
   return (
     <div className="bg-gray-900">
-      <Navbar/>
+      <Navbar />
       <Hero />
       <AboutUs />
       <OurServices />
-      <MyProjects /> {/* Corrected component usage */}
-      <ContactMe/>
+      <MyProjects />
+      <ContactMe />
     </div>
   );
 }
