3f1c00b052b7cb54cd99cf52f5df4106d0eef667
Studio-m2 <maryamnaeem123456789@gmail.com>
2024-08-29 12:39:57 +0500

    updateds

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,11 @@ import Image from 'next/image';
 import PricingCards from '@/components/pricing-templete/pricing';
 import Signinform from '@/components/signinform-templete/signin';
 import Signupform  from '@/components/signupform-templete/signup';
-//import PricingTable from '@/components/pricingtable-templete/pricingtable';
+
 const page = () => {
   return (
 <>
 
-<PricingCards/>
-<Signupform/>
-<Signinform/>
 </>
 
 
